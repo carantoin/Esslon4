c91dfd016d861548c9566fe4e6ced87a251eed9e
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-10 11:20:58 +0200

    Initial commit

---
+++ Esslon4/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Console.WriteLine("Введите трёхзначное число:");
+    int a = int.Parse(Console.ReadLine());
+    if (a < 100 || a > 999)
+    {
+        Console.WriteLine("Введите трёхзначное число!");
+    }
+    else
+    {
+        int n = (a % 100) * 10 + a / 100;
+        Console.WriteLine($"n={n}");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите правильные данные!");
+}
